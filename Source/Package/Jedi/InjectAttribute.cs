6bb8cf538af692d4a6c19c2aef77c283cdd263a8
Dishan Philips <dishanphilips@gmail.com>
2020-09-28 11:27:45 +0400

    [CODE] - Added Injection and Optionalnject attributes.

---
+++ Source/Package/Jedi/InjectAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jedi
+{
+    [AttributeUsage(AttributeTargets.Constructor|AttributeTargets.Field|AttributeTargets.Property|AttributeTargets.Method)]
+    public class InjectAttribute : Attribute
+    {
+        /// <summary>
+        /// The Id to be used when injecing
+        /// </summary>
+        public object Id { get; }
+
+        /// <summary>
+        /// If true, no exceptions will be thrown when injecting
+        /// </summary>
+        public bool Optional { get; }
+
+
+        /// <summary>
+        /// Jedi injects when placed on a Constructor/Field/Property/Method
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="optional"></param>
+        public InjectAttribute(object id = null, bool optional = false)
+        {
+            Id = id;
+            Optional = optional;
+        }
+    }
+}
---
+++ Source/Package/Jedi/InjectOptionalAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jedi
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Method)]
+    public class InjectOptionalAttribute : InjectAttribute
+    {
+        /// <summary>
+        /// A shorthand attribute for optional injection
+        /// </summary>
+        /// <param name="id"></param>
+        public InjectOptionalAttribute(object id = null) : base(id, true)
+        { 
+        }
+    }
+}
